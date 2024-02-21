a5e0cbfe03bb188c0370f2d9fe5214765c9d7876
TonyGrif <TonyGriffin2000@gmail.com>
2024-02-21 14:02:50 -0500

    create log files

--- get-timemaps.sh
+++ get-timemaps.sh
@@ -20,10 +20,19 @@ if [[ ! -d "timemaps" ]]; then
     mkdir timemaps
 fi
 
+if [[ ! -d "timemaps/logs" ]]; then
+    echo "Creating logs directory"
+    mkdir timemaps/logs
+fi
+
 while read line; do
     ./memgator-linux-amd64 \
         -a https://raw.githubusercontent.com/odu-cs432-websci/public/main/archives.json \
+        -b "timemaps/logs/benchmarks.txt" \
         -c "tgrif007@odu.edu" \
-        -F 2 -f JSON -V "$line"
+        -F 2 \
+        -f JSON \
+        -l "timemaps/logs/info.txt" \
+        "$line"
     sleep 30
 done < $1
