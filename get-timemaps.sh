b165618cfce716239900868c89665766b3f4bc96
TonyGrif <TonyGriffin2000@gmail.com>
2024-02-21 13:40:13 -0500

    run memgator for each line in file

--- get-timemaps.sh
+++ get-timemaps.sh
@@ -21,5 +21,6 @@ if [[ ! -d "timemaps" ]]; then
 fi
 
 while read line; do
-    echo "$line"
+    ./memgator-linux-amd64 -a https://raw.githubusercontent.com/odu-cs432-websci/public/main/archives.json -c "tgrif007@odu.edu" -F 2 -f JSON -V "$line"
+    sleep 30
 done < $1
