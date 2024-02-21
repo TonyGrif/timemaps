fe2597730204083ad8f48a5e42d6a5165f8ae754
TonyGrif <TonyGriffin2000@gmail.com>
2024-02-21 13:45:03 -0500

    reformat command line

--- get-timemaps.sh
+++ get-timemaps.sh
@@ -21,6 +21,9 @@ if [[ ! -d "timemaps" ]]; then
 fi
 
 while read line; do
-    ./memgator-linux-amd64 -a https://raw.githubusercontent.com/odu-cs432-websci/public/main/archives.json -c "tgrif007@odu.edu" -F 2 -f JSON -V "$line"
+    ./memgator-linux-amd64 \
+        -a https://raw.githubusercontent.com/odu-cs432-websci/public/main/archives.json \
+        -c "tgrif007@odu.edu" \
+        -F 2 -f JSON -V "$line"
     sleep 30
 done < $1
